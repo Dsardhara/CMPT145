2120d8204e3abc64116a5d30b5e2ffc83c587157
Dsardhara <dsardhara1234@gmail.com>
2023-06-17 05:42:09 -0600

    file name changed from whereswrong to a2q3

--- a2q3.py
+++ a2q3.py
@@ -1,25 +1,10 @@
-###############################################################################
-# CMPT 145 Course material
-# Original Author: Lauresa Stilling
-# Date Created:   31 May 2023
-# Last Edited:    31 May 2023
-#
-# All rights reserved.
-#
-# This document contains resources for homework assigned to students of
-# CMPT 145 and shall not be distributed without permission.  Posting this
-# file to a public or private website, or providing this file to a person
-# not registered in CMPT 145, constitutes Academic Misconduct, according
-# to the University of Saskatchewan Policy on Academic Misconduct.
-#
-# Synopsis:
-#    Debugging exercise, relevant to Chapter 3 and 7
-###############################################################################
-
-# TODO: Fill in your information below
-# Student Name
-# NSID
-# Student Number
+# Name: Darshan Sardhara
+# Instructor Name: Lauresa
+# NSID: WLZ670
+# Course Name: CMPT 145
+# Student ID: 11355677
+# section Number: 01
+# Course number: 41442
 
 # Recall another name for a list of dictionaries is a record.
 
