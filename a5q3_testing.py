b3b271e3f6042c67cdde9240794a3984e002609d
Dsardhara <dsardhara1234@gmail.com>
2023-08-07 22:12:05 -0600

    a5q3_test : i made half way.the test cases has been made for third function

---
+++ a5q3_testing.py
@@ -0,0 +1,38 @@
+# Name: Darshan Sardhara
+# Instructor Name: Lauresa
+# NSID: WLZ670
+# Course Name: CMPT 145
+# Student ID: 11355677
+# section Number: 01
+
+import a5q3 as file
+from treenode import Treenode
+
+# Test cases for ordered() function
+
+test_item = 'ordered()'
+tnode = None
+expected = True
+reason = 'Empty Tree'
+result = file.ordered(tnode)
+if result != expected:
+    print('Test failed: {}: got "{}" expected "{}" -- {}'.format(test_item, result, expected, reason))
+
+test_item = 'ordered()'
+tnode = Treenode(3)
+expected = True
+reason = 'Single Tree node'
+result = file.ordered(tnode)
+if result != expected:
+    print('Test failed: {}: got "{}" expected "{}" -- {}'.format(test_item, result, expected, reason))
+
+test_item = 'ordered()'
+tnode = Treenode(4,
+                   Treenode(2, Treenode(1), Treenode(3)),
+                   Treenode(6, Treenode(5), Treenode(7)))
+expected = True
+reason = 'Perfect ordered Tree'
+result = file.ordered(tnode)
+if result != expected:
+    print('Test failed: {}: got "{}" expected "{}" -- {}'.format(test_item, result, expected, reason))
+
