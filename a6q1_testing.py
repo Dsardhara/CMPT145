fe765d10c01cb42af6d807aacfe8e74ce646c71b
Dsardhara <dsardhara1234@gmail.com>
2023-08-18 15:37:31 -0600

    a6q1-test : this time i change my method to test cases. i found new thing path method. it is very useful in test case.

---
+++ a6q1_testing.py
@@ -0,0 +1,24 @@
+# Name: Darshan Sardhara
+# Instructor Name: Lauresa
+# NSID: WLZ670
+# Course Name: CMPT 145
+# Student ID: 11355677
+# section Number: 01
+
+from unittest.mock import patch
+
+from a6q1 import Conway
+
+ls = []
+# Test1
+test = "Input1.txt"
+expected = ['*-*', '*-*', '*-*']
+with patch("builtins.input", side_effect=["n"]):
+    Conway(test, 1)
+with open("input1_1steps.txt", "r") as file:
+    for i in file:
+        ls.append(i.removesuffix('\n'))
+if ls != expected:
+    print("Testing Conway() with", test, "\nExpected:", expected, "\nGot:", ls)
+else:
+    print("Test1 Success...")
