0a3aa9cdbc5b3afa52a205ab1e71b1befc1d761a
Dsardhara <dsardhara1234@gmail.com>
2023-08-18 15:02:04 -0600

    a6q1 : with removing minor mistake now code run perfectly. it reads data from file and check the neighbout and zombie and the also writes in a file.

--- a6q1.py
+++ a6q1.py
@@ -11,7 +11,7 @@ ls = []
 
 
 def Conway(fileName, repetation=3):
-    global len_vertical, line, y, x, f_name, flag, main_repetation
+    global len_vertical, line, y, x, f_name, flag, main_repetation, arr
     main_repetation = repetation
     f_name = fileName.removesuffix('.txt')
 
@@ -34,7 +34,7 @@ def Conway(fileName, repetation=3):
     y = count
     x = len(line)
     arr = np.array(ls).reshape(y, x)
-    print('original array:\n', arr)
+    # print('original array:\n', arr)
     f.close()
     checkNeighbour(arr, repetation)
     zombie_neighbour(arr)
@@ -49,41 +49,41 @@ def checkNeighbour(arr1, repetation):
             if (arr1[i - 1][j] == '*' or arr1[i - 1][j] == 'Z') and i > 0:
                 aCount += 1
             # left
-            if (arr1[i][j - 1] == '*' or arr1[i][j - 1] == 'Z') and j > 0:
+            if arr1[i][j - 1] == '*' and j > 0:
                 aCount += 1
             # bottom
             try:
-                if (arr1[i + 1][j] == '*' or arr1[i + 1][j] == 'Z') and i < y - 1:
+                if arr1[i + 1][j] == '*' and i < y - 1:
                     aCount += 1
             except IndexError:
                 pass
             # right
             try:
-                if (arr1[i][j + 1] == '*' or arr1[i][j + 1] == 'Z') and j < x - 1:
+                if arr1[i][j + 1] == '*' and j < x - 1:
                     aCount += 1
             except IndexError:
                 pass
             # top-left
             try:
-                if (arr1[i - 1][j - 1] == '*' or arr1[i - 1][j - 1] == 'Z') and i > 0 and j > 0:
+                if arr1[i - 1][j - 1] == '*' and i > 0 and j > 0:
                     aCount += 1
             except IndexError:
                 pass
             # bottom-left
             try:
-                if (arr1[i + 1][j - 1] == '*' or arr1[i + 1][j - 1] == 'Z') and i < y - 1 and j > 0:
+                if arr1[i + 1][j - 1] == '*' and i < y - 1 and j > 0:
                     aCount += 1
             except IndexError:
                 pass
             # top_right
             try:
-                if (arr1[i - 1][j + 1] == '*' or arr1[i - 1][j + 1] == 'Z') and i > 0 and j < x - 1:
+                if arr1[i - 1][j + 1] == '*' and i > 0 and j < x - 1:
                     aCount += 1
             except IndexError:
                 pass
             # bottom-right
             try:
-                if (arr1[i + 1][j + 1] == '*' or arr1[i + 1][j + 1] == 'Z') and i < y - 1 and j < x - 1:
+                if arr1[i + 1][j + 1] == '*' and i < y - 1 and j < x - 1:
                     aCount += 1
             except IndexError:
                 pass
@@ -96,23 +96,87 @@ def checkNeighbour(arr1, repetation):
         game_Of_Life(arr1, count_arr, repetation)
 
 
+def zombie_neighbour(arr2):
+    zombie_list = []
+
+    for i in range(len(arr2)):
+        for j in range(len(arr2[i])):
+            zombies_increment = 0
+
+            # Top
+            if arr2[i - 1][j] == 'Z' and i > 0:
+                zombies_increment += 1
+
+            # Top-Left
+            try:
+                if arr2[i - 1][j - 1] == 'Z' and i > 0 and j > 0:
+                    zombies_increment += 1
+            except IndexError:
+                pass
+
+            # Left
+            if arr2[i][j - 1] == 'Z' and j > 0:
+                zombies_increment += 1
+
+            # Bottom-Left
+            try:
+                if arr2[i + 1][j - 1] == 'Z' and i < y - 1 and j > 0:
+                    zombies_increment += 1
+            except IndexError:
+                pass
+
+            # Bottom
+            try:
+                if arr2[i + 1][j] == 'Z' and i < y - 1:
+                    zombies_increment += 1
+            except IndexError:
+                pass
+
+            # Bottom-Right
+            try:
+                if arr2[i + 1][j + 1] == 'Z' and i < y - 1 and j < x - 1:
+                    zombies_increment += 1
+            except IndexError:
+                pass
+
+            # Right
+            try:
+                if arr2[i][j + 1] == 'Z' and j < x - 1:
+                    zombies_increment += 1
+            except IndexError:
+                pass
+
+            # Top-Right
+            try:
+                if arr2[i - 1][j + 1] == 'Z' and i > 0 and j < x - 1:
+                    zombies_increment += 1
+            except IndexError:
+                pass
+            zombie_list.append(zombies_increment)
+    zombie_arr = np.array(zombie_list).reshape(y, x)
+    return zombie_arr
+
+
 def game_Of_Life(original_arr, list_arr, repetation):
     last_ls = []
-
+    call_zombie = zombie_neighbour(original_arr)
     for i in range(len(original_arr)):
         for j in range(len(original_arr[i])):
-            if (list_arr[i][j] < 2 or list_arr[i][j] > 3) and original_arr[i][j] == '*':
+            living_count = list_arr[i][j] + call_zombie[i][j]
+            if living_count < 2 or living_count > 3:
                 last_ls.append('-')
-            elif (list_arr[i][j] == 2 or list_arr[i][j] == 3) and original_arr[i][j] == '*':
+            elif (living_count == 2 or living_count == 3) and original_arr[i][j] == '*' and call_zombie[i][j] > 0:
+                last_ls.append('Z')
+            elif (living_count == 2 or living_count == 3) and original_arr[i][j] == '*':
                 last_ls.append('*')
-            elif list_arr[i][j] == 3 and original_arr[i][j] == '*':
-                last_ls.append('*')
-            elif (list_arr[i][j] == 3 and original_arr[i][j] == '-') or original_arr[i][j] == 'Z':
+            elif original_arr[i][j] == 'Z':
+                last_ls.append('Z')
+            elif living_count == 3:
                 last_ls.append('Z')
             else:
                 last_ls.append('-')
     last_arr = np.array(last_ls).reshape(y, x)
-    print('one step forward:\n', last_arr)
+    # print('one step forward:\n', last_arr)
 
     if flag == True:
         print("\nIteration -", main_repetation - repetation)
