20db1805123408db823114944cbf9bd6ebacd842
Dsardhara <dsardhara1234@gmail.com>
2023-08-18 22:01:17 -0600

    a6q2 - i made first two method is_empty and size in container class.

---
+++ a6q2_Container.py
@@ -0,0 +1,19 @@
+# Name: Darshan Sardhara
+# Instructor Name: Lauresa
+# NSID: WLZ670
+# Course Name: CMPT 145
+# Student ID: 11355677
+# section Number: 01
+# Course number: 41442
+
+# a6q2_Container.py
+
+class Container:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
