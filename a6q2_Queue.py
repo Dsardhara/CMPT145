369ce8f5a4efdffcec790579bf7a740daad14ae9
Dsardhara <dsardhara1234@gmail.com>
2023-08-19 02:20:50 -0600

    a6q2_queue - till now i just made only 2 method size and peek rest of them are remaining facing problem in other method

--- a6q2_Queue.py
+++ a6q2_Queue.py
@@ -9,32 +9,13 @@
 from a6q2_Container import Container
 
 class Queue(Container):
-    def __init__(self):
+    def _init_(self):
         super().__init__()
-        self.__head = 0
-        self.__tail = 0
+        self._head = None
+        self._tail = None
 
     def size(self):
         return self.get_size()
 
-    def enqueue(self, data):
-        # new_node = N.Node(data, None)
-        self.push_value(data)
-        if self._size == 1:
-            self.__head = self._first
-        self.__tail = self._first
-
-    def dequeue(self):
-        if self._first is None:
-            return None
-        data = self.__head.get_data()
-        self.__head = self.__head.get_next()
-        self._size -= 1
-        if self.__head is None:
-            self.__tail = None
-        return data
-
     def peek(self):
-        first_node = self.__head
-        result = first_node.get_data()
-        return result
+        return self._head.get_data()
