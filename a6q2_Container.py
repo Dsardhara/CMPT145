82a45f49fb5386b2ac5c5d02e4fc03401855d1ca
Dsardhara <dsardhara1234@gmail.com>
2023-08-19 00:06:38 -0600

    a6q2 - container file has been made properly as instruction given. all method is working properly with help of node file.

--- a6q2_Container.py
+++ a6q2_Container.py
@@ -6,14 +6,36 @@
 # section Number: 01
 # Course number: 41442
 
-# a6q2_Container.py
+import node as N
+
 
 class Container:
     def __init__(self):
-        self.items = []
+        self.__size = 0
+        self.__first = None
 
     def is_empty(self):
-        return len(self.items) == 0
+        return self.__size == 0
 
-    def size(self):
-        return len(self.items)
+    def get_size(self):
+        return self.__size
+
+    def push_value(self, data):
+        n_node = N.Node(data)
+        if self.__first is None:
+            self.__first = n_node
+        else:
+            n_node.set_next(self.__first)
+            self.__first = n_node
+        self.__size += 1
+
+    def pop_value(self):
+        if self.__size == 0:
+            return None
+        value = self.__first.get_data()
+        self.__first = self.__first.get_next()
+        self.__size -= 1
+        return value
+
+    def peek_value(self):
+        return self.__first.get_data()
