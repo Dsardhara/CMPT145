9ef66d891ec7a76847203662556fb7e6010ff21b
Dsardhara <dsardhara1234@gmail.com>
2023-08-19 02:14:50 -0600

    a6q2_container - i just added docstring to container file.

--- a6q2_Container.py
+++ a6q2_Container.py
@@ -1,41 +1,66 @@
-# Name: Darshan Sardhara
-# Instructor Name: Lauresa
-# NSID: WLZ670
-# Course Name: CMPT 145
-# Student ID: 11355677
-# section Number: 01
-# Course number: 41442
-
 import node as N
 
-
 class Container:
+
     def __init__(self):
-        self.__size = 0
-        self.__first = None
+        """
+        Initializes an empty container.
+        """
+        self._size = 0
+        self._first = None
 
     def is_empty(self):
-        return self.__size == 0
+        """
+        Checks if the container is empty.
+
+        Returns:
+            bool: True if the container is empty, False otherwise.
+        """
+        return self._size == 0
 
     def get_size(self):
-        return self.__size
+        """
+        Returns the number of elements in the container.
+
+        Returns:
+            int: The number of elements in the container.
+        """
+        return self._size
 
     def push_value(self, data):
+        """
+        Adds a new value to the beginning of the container.
+
+        Args:
+            data: The value to be added to the container.
+        """
         n_node = N.Node(data)
-        if self.__first is None:
-            self.__first = n_node
+        if self._first is None:
+            self._first = n_node
         else:
-            n_node.set_next(self.__first)
-            self.__first = n_node
-        self.__size += 1
+            n_node.set_next(self._first)
+            self._first = n_node
+        self._size += 1
 
     def pop_value(self):
-        if self.__size == 0:
+        """
+        Removes and returns the value from the beginning of the container.
+
+        Returns:
+            The value removed from the container, or None if the container is empty.
+        """
+        if self._size == 0:
             return None
-        value = self.__first.get_data()
-        self.__first = self.__first.get_next()
-        self.__size -= 1
+        value = self._first.get_data()
+        self._first = self._first.get_next()
+        self._size -= 1
         return value
 
     def peek_value(self):
-        return self.__first.get_data()
+        """
+        Returns the value from the beginning of the container without removing it.
+
+        Returns:
+            The value from the beginning of the container, or None if the container is empty.
+        """
+        return self._first.get_data()
