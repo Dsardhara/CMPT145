baa84f7ac81258d6981034955ec77569fb12d74f
Dsardhara <dsardhara1234@gmail.com>
2023-08-19 02:07:07 -0600

    a6q2_Stack : i just added docstring for each function

--- a6q2_Stack.py
+++ a6q2_Stack.py
@@ -9,16 +9,58 @@
 from a6q2_Container import Container
 class Stack(Container):
     def __init__(self):
+        """
+        Purpose :creates an empty stack
+        """
         super().__init__()
 
     def size(self):
+        """
+        Purpose
+            returns the number of data values in the stack
+        Return:
+            The number of data values in the stack
+        """
+
         return super().get_size()
 
     def push(self, data):
+        """
+        Purpose
+            adds the given data value to the stack
+        Pre-conditions:
+            value: data to be added
+        Post-condition:
+            the value is added to the stack
+        Return:
+            (none)
+        """
+
         super().push_value(data)
 
     def peek(self):
+        """
+        Purpose
+            returns the value from the top of given stack
+            without removing it
+            Note: the stack cannot be empty!
+        Post-condition:
+            None
+        Return:
+            the value at the top of the stack
+        """
+
         return super().peek_value()
 
     def pop(self):
+        """
+        Purpose
+            Removes and returns a data value from the stack.
+            Note: the stack cannot be empty!
+        Post-condition:
+            the first value is removed from the stack
+        Return:
+            the first value in the stack, or None
+        """
+
         return super().pop_value()
