7a84880fb05815ccb4f982c3eb69cb0c1a8ea9f8
Dsardhara <dsardhara1234@gmail.com>
2023-08-19 12:36:46 -0600

    a6q2_queue - last two remaining method has been made.

--- a6q2_Queue.py
+++ a6q2_Queue.py
@@ -7,7 +7,7 @@
 # Course number: 41442
 
 from a6q2_Container import Container
-
+import node as N
 class Queue(Container):
     def _init_(self):
         super().__init__()
@@ -16,6 +16,26 @@ class Queue(Container):
 
     def size(self):
         return self.get_size()
+
+    def enqueue(self, data):
+        new_node = N.Node(data)
+        if self._first is None:
+            self._first = new_node
+            self._head = self._first
+        else:
+            self._tail.set_next(new_node)
+        self._tail = new_node
+        self._size += 1
+
+    def dequeue(self):
+        if self._head is None:
+            return None
+        data = self._head.get_data()
+        self._head = self._head.get_next()
+        self._size -= 1
+        if self._head is None:
+            self._tail = None
+        return data
 
     def peek(self):
         return self._head.get_data()
